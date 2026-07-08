e8dd78b0c8409070f5f8df5c7d0a3ecc0b555c1f
vridhi <rajeevvridhi@gmail.com>
2026-07-08 17:46:01 +0530

    Add user and message models, implement MongoDB connection, and update dependencies

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,25 @@
 import express from "express";
 import "dotenv/config" ;
+import User from "./models/user.model.js";
+import { connect } from "mongoose";
+import { connectDb } from "./lib/db.js";
 
+import {clerkMiddleware} from "@clerk/express"
+import cors from "cors"
 
 const app = express(); 
 const PORT = process.env.PORT ;
+const FRONTEND_URL = process.env.FRONTEND_URL ;
+//middlewares
+app.use(express.json());
+app.use(cors({origin:FRONTEND_URL , credentials : true}));
+app.use(clerkMiddleware());
+
+app.get("/health", (req,res)=>{
+    res.status(200).json({ok :true});
+});
 
 app.listen(PORT , ()=>{
+    connectDb();
     console.log('server is runnning on port:' , PORT)
 })
---
+++ backend/src/lib/db.js
@@ -0,0 +1,16 @@
+import mongoose, { mongo } from "mongoose";
+
+export async function connectDb(params) {
+    try{
+        const mongoUri = process.env.MONGO_URI 
+        if(!mongoUri){
+            throw new Error("MONGO_URI is required")
+        }
+        const conn = await mongoose.connect(mongoUri);
+        console.log("mongoDB connected:" , conn.connection.host);
+    }
+    catch(error){
+        console.log("mongoDB connection error:" , error.message);
+        process.exit(1);
+    }
+}
