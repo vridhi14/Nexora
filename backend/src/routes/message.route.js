c243d6a8ad56bde53c9272c0cb609d02d460a796
vridhi <rajeevvridhi@gmail.com>
2026-07-18 23:21:42 +0530

    fix: reorganize imports for better structure and readability in message route

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,18 +1,20 @@
-import express from "express"; 
-import { getUsersForSidebar } from "../controllers/message.controller.js";
-import { getConversationsForSidebar } from "../controllers/message.controller.js";
-import { getMessages } from "../controllers/message.controller.js";
+import express from "express";
+import {
+  getConversationsForSidebar,
+  getMessages,
+  getUsersForSidebar,
+  sendMessage,
+} from "../controllers/message.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import {upload} from "../middleware/upload.middleware.js";
-import { sendMessage } from "../controllers/message.controller.js";
+import { upload } from "../middleware/upload.middleware.js";
 
 const router = express.Router();
 
 router.use(protectRoute);
 
-router.get("/users" , getUsersForSidebar);
-router.get("/conversations" ,getConversationsForSidebar);
-router.get("/:id" ,getMessages);
-router.post("/send/:id" ,upload.single("media") ,sendMessage);
+router.get("/users", getUsersForSidebar);
+router.get("/conversations", getConversationsForSidebar);
+router.get("/:id", getMessages);
+router.post("/send/:id", upload.single("media"), sendMessage);
 
-export default router ; 
+export default router;
