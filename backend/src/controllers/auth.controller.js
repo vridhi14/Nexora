48a18ed126a142238ab0aeb078562b0048819d06
vridhi <rajeevvridhi@gmail.com>
2026-07-18 23:23:41 +0530

    fix: improve formatting and readability in auth controller and middleware

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,7 @@
-export async function checkAuth(req,res,next) {
-    
-    if(!req.user){
-        return res.status(401).json({message:"Unauthorized"});
-    }
-    res.status(200).json(req.user);
+export async function checkAuth(req, res, next) {
+  if (!req.user) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  res.status(200).json(req.user);
 }
