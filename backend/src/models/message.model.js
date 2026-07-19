dc1330f2e840abed83f74498d9e9d866dbffa47a
vridhi <rajeevvridhi@gmail.com>
2026-07-19 15:28:03 +0530

    fix: correct formatting and improve readability in message and user models

--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -20,7 +20,7 @@ const messageSchema = new mongoose.Schema({
         type:"String"
     },
 
-} , {timeseries:true});
+} , { timestamps: true });
 
 
 const Message = mongoose.model("Message" , messageSchema);
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,28 +1,29 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-
-    clerkId : {
-        type:"String", 
-        required : true , 
-        unique:true ,
+const userSchema = new mongoose.Schema(
+  {
+    clerkId: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    email :{
-        type:"String",
-        required : true , 
-        unique : true ,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    fullName :{
-        type:"String",
-        required : true ,  
+    fullName: {
+      type: String,
+      required: true,
     },
-    profilePic :{
-        type:"String",
-        default : "" ,
-    }
-} , 
-{timestamps:true}
+    profilePic: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }, // createdAt & updatedAt
 );
 
-const User = mongoose.model("User" , userSchema);
-export default User ; 
+const User = mongoose.model("User", userSchema);
+
+export default User;
